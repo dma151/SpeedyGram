0236dab8d7021f9f2d9aa0cccb5c210c46407201
deon <mkandaniel@gmail.com>
2022-07-28 20:48:30 -0700

    added user validation to crud functionalty

--- controllers/commentController.js
+++ controllers/commentController.js
@@ -1,30 +1,44 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const Comment = require('../models/Comment')
+const Comment = require("../models/Comment");
 
-router.get('/', async (req, res) => {
-    const comments = await Comment.find({})
-    res.status(200).json({data: comments})
-})
+router.get("/", async (req, res) => {
+  const comments = await Comment.find({});
+  res.status(200).json({ data: comments });
+});
 
-router.get('/:id', async (req, res) => {
-    const comment = await Comment.find({_id:req.params.id})
-    res.status(200).json({data: comment})
-})
+router.get("/:id", async (req, res) => {
+  const comment = await Comment.find({ _id: req.params.id });
+  res.status(200).json({ data: comment });
+});
 
-router.post('/new', async (req, res) => {
-    const newComment = await Comment.create(req.body)
-    res.status(201).json({newComment: newComment})
-})
+router.post("/new", async (req, res) => {
+  const newComment = await Comment.create(req.body);
+  res.status(201).json({ newComment: newComment });
+});
 
-router.put('/:id', async (req, res) => {
-    const editComment = await Comment.findByIdAndUpdate(req.params.id, req.body, {new: true})
-    res.status(200).json({editedComment: editComment})
-})
+router.put("/:id", async (req, res) => {
+  const ownerCheck = await Comment.find({ _id: req.params.id });
+  if (ownerCheck.email == req.body.email) {
+    const editComment = await Comment.findByIdAndUpdate(
+      req.params.id,
+      req.body,
+      { new: true }
+    );
+    res.status(200).json({ editedComment: editComment });
+  } else {
+    res.status(400).json("Unauthorized: You are not the creator of this comment");
+  }
+});
 
-router.delete('/:id', async (req, res) => {
-    const deleteComment = await Comment.findByIdAndDelete(req.params.id)
-    res.status(204).json("Successfully deleted")
-})
+router.delete("/:id", async (req, res) => {
+  const ownerCheck = await Comment.find({ _id: req.params.id });
+  if (ownerCheck.email == req.body.email) {
+    const deleteComment = await Comment.findByIdAndDelete(req.params.id);
+    res.status(204).json("Successfully deleted");
+  } else {
+    res.status(400).json("Unauthorized: You are not the creator of this comment");
+  }
+});
 
-module.exports = router
+module.exports = router;
--- controllers/postController.js
+++ controllers/postController.js
@@ -8,21 +8,36 @@ router.get("/", async (req, res) => {
   res.status(200).json({ posts: posts });
 });
 
+router.get("/myposts", async (req, res) => {
+  const posts = await Post.find({ email: req.body.email }).populate("comments");
+  res.status(200).json({ posts: posts });
+});
+
 router.post("/new", async (req, res) => {
   const post = await Post.create(req.body);
   res.status(201).json({ newPost: post });
 });
 
 router.put("/:id", async (req, res) => {
-  const editPost = await Post.findByIdAndUpdate(req.params.id, req.body, {
-    new: true,
-  });
-  res.status(200).json({ editedPost: editPost });
+  const ownerCheck = await Post.find({ _id: req.params.id });
+  if (ownerCheck.email == req.body.email) {
+    const editPost = await Post.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+    });
+    res.status(200).json({ editedPost: editPost });
+  } else {
+    res.status(400).json("Unauthorized: You are not the creator of this post")
+  }
 });
 
 router.delete("/:id", async (req, res) => {
-  const deletePost = await Post.findByIdAndDelete(req.params.id);
-  res.status(204).json("Successfully deleted");
+  const ownerCheck = await Post.find({ _id: req.params.id });
+  if (ownerCheck.email == req.body.email) {
+    const deletePost = await Post.findByIdAndDelete(req.params.id);
+    res.status(204).json("Successfully deleted");
+  } else {
+    res.status(400).json("Unauthorized: You are not the creator of this post")
+  }
 });
 
 // Add new comment to current post
